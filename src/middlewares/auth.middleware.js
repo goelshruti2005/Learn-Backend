b6b3aa64869dd759a79ae175a8fe145432f93b04
Shruti Goel <analytic.girl1404@gmail.com>
2024-12-16 09:36:20 +0530

    add user account management endpoints

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -7,7 +7,7 @@ import {User} from "../models/user.modules.js"
 export const verifyJWT = asyncHandler(async(req,res,next)=>{
    try {
     const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
-    const token = /*req.cookies?.accessToken ||*/ req.header("Authorization")?.replace("Bearer ","")
+   //const token = /*req.cookies?.accessToken ||*/ req.header("Authorization")?.replace("Bearer ","")
     //console.log("req :",req);
     // console.log("header :",req.header("Authorization").replace("Bearer ",""));
     
